03e2085398265da2d33b58436f2b1f76f4cd036b
smilence86 <smilence86@gmail.com>
2023-01-03 10:45:12 +0800

    chia_tracker

--- src/notifier/telegram.js
+++ src/notifier/telegram.js
@@ -11,13 +11,14 @@ class Telegram{
             .replace(/\</g, '\\<')
             .replace(/\>/g, '\\>')
             .replace(/\+/g, '\\+')
-            .replace(/\,/g, '\\,');
+            .replace(/\,/g, '\\,')
+            .replace(/\!/g, '\\!');
         const now = new Date();
         try {
             const url = `https://api.telegram.org/bot${token}/sendMessage`;
             // console.info(url);
             const res = await request.post(url)
-                // .set('Content-type', 'application/x-www-form-urlencoded')
+                .set('Content-type', 'application/x-www-form-urlencoded')
                 .send({
                     parse_mode: 'MarkdownV2',
                     chat_id,
@@ -25,10 +26,10 @@ class Telegram{
                 }).timeout(10 * 1000).retry(5);
             console.log(`\n${res.text}`);
         } catch (e) {
-            console.info(`------------- send to telegram failed ${now} --------------`);
+            console.info(`------------- send to telegram failed ${now.toISOString()} --------------`);
             console.info(`chat_id: ${chat_id}`);
             console.info(`content: ${content}`);
-            console.error(e.stack || e);
+            console.error(e?.response?.text || e);
         }
     }
 
